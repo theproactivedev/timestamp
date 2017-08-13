0ae995387d0ef8d1ce4c2ce3157dbb0d20565a0e
System <none>
2017-08-13 08:55:25 +0000

    Checkpoint

--- public/app.js
+++ public/app.js
@@ -1,24 +1,22 @@
-// var express = require("express");
-// var bodyParser = require("body-parser");
-// var cors = require("cors");
+var express = require("express");
+var bodyParser = require("body-parser");
+var cors = require("cors");
 
-// var app = module.exports = express();
-// app.use(bodyParser.json());
-// app.use(cors());
+var app = module.exports = express();
+app.use(bodyParser.json());
+app.use(cors());
 
-
-
-
-
-
-
-
-// app.listen(3000, function(error) {
-//   if (error) throw error;
+app.get("/", function(req, res) {
   
-//   console.log("Working");
-// });
+});
 
-var msg = document.getElementById("message");
 
-msg.html("<p>Hi!</p>");
+
+
+
+
+app.listen(3000, function(error) {
+  if (error) throw error;
+  
+  console.log("Working");
+});
