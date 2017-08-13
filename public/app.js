fda9e3683e0c9d0b59b17b876c55f8b46385f088
System <none>
2017-08-13 07:38:24 +0000

    Checkpoint

---
+++ public/app.js
@@ -0,0 +1,24 @@
+// var express = require("express");
+// var bodyParser = require("body-parser");
+// var cors = require("cors");
+
+// var app = module.exports = express();
+// app.use(bodyParser.json());
+// app.use(cors());
+
+
+
+
+
+
+
+
+// app.listen(3000, function(error) {
+//   if (error) throw error;
+  
+//   console.log("Working");
+// });
+
+var msg = document.getElementById("message");
+
+msg.html("<p>Hi!</p>");
