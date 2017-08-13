6ad47dc61c4e7a8f13587c677d759ae03922d842
System <none>
2017-08-13 05:37:24 +0000

    Checkpoint

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var cors = require("cors");
+
+var app = module.exports = express();
+app.use(bodyParser.json());
+app.use(cors());
+
