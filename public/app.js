1fdb22ada8d8e82fb19ae12b4e06425beb9456fc
System <none>
2017-08-13 10:23:25 +0000

    Checkpoint

--- public/app.js
+++ public/app.js
@@ -7,10 +7,14 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.get("/timestamp/:dateParams", function(req, res) {
-    res.type('txt').send('Home found');
+    // res.type('txt').send('Home found');
   
   
-  var parameter = req.params.dateParams;+
+  var parameter = req.params.dateParams;
+  
+  if (parameter
+  
+  
 });
 
 
@@ -23,3 +27,34 @@ app.listen(3000, function(error) {
   
   console.log("Working");
 });
+
+
+// var http = require("http");
+// var url = require("url");
+
+// var server = http.createServer(function(request, response) {
+  
+//   var result = url.parse(request.url, true);
+//   var date = new Date(result.query.iso);
+  
+//   if (result.pathname === "/api/parsetime") {
+//       var timeObj = {
+//         "hour" : date.getHours(),
+//         "minute" : date.getMinutes(),
+//         "second" : date.getSeconds()
+//       };
+//   } else if (result.pathname === "/api/unixtime") {
+//     var timeObj = {
+//       "unixtime" : date.getTime()
+//     };
+//   }
+  
+//   if (timeObj) {
+//     response.writeHead(200, { 'Content-Type': 'application/json' });
+//     response.end(JSON.stringify(timeObj));
+//   }
+   
+
+// });
+
+// server.listen(Number(process.argv[2]));
