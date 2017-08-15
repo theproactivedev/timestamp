e7ebd7fae113c6c3b47da169340f32371f72d539
System <none>
2017-08-15 15:19:44 +0000

    Fixed code

--- server.js
+++ server.js
@@ -8,7 +8,6 @@
 var fs = require('fs');
 var express = require('express');
 var app = express();
-var dateFormat = require("dateformat");
 
 if (!process.env.DISABLE_XORIGIN) {
   app.use(function(req, res, next) {
